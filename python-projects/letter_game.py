975a3393f4bd8b236d967a5f03666388ae8e46a1
Yoshi275 <ngyoshi275@gmail.com>
2017-11-11 20:44:00 +0800

    Adding letter_game.py into treehouse projects

---
+++ python-projects/letter_game.py
@@ -0,0 +1,115 @@
+import random
+import os
+import sys
+
+def play(done):
+    clear()
+    secret_word = random.choice(words)
+    bad_guesses = []
+    good_guesses = []
+    
+    while True:
+        draw(bad_guesses, good_guesses, secret_word)
+        guess = get_guess(bad_guesses, good_guesses)
+        
+        if guess in secret_word:
+            good_guesses.append(guess)
+            found = True
+            for letter in secret_word:
+                if letter not in good_guesses:
+                    found = False
+            if found:
+                print("You win!")
+                print("The secret word was {}.".format(secret_word))
+                done = True
+        else:
+            bad_guesses.append(guess)
+            if len(bad_guesses) == 7:
+                draw(bad_guesses, good_guesses, secret_word)
+                print("You lost!")
+                print("The secret word was {}.".format(secret_word))
+                done = True
+        if done:
+            play_again = input("Play again? Y/n : ")
+            if play_again.lower() != 'n':
+                done = False
+            else:
+                sys.exit()
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def draw(bad_guesses, good_guesses, secret_word):
+    clear()
+    
+    print('Strikes: {}/7'.format(len(bad_guesses)))
+    print(' ')
+    for bad_guess in bad_guesses:
+        print(bad_guess, end = ' ')
+    print('\n\n')
+    
+    # draw guessed letters, spaces and strikes
+    for letter in secret_word:
+        if letter in good_guesses:
+            print(letter, end = ' ')
+        else:
+            print("_", end = ' ')
+            
+    print(' ')
+
+def get_guess(bad_guesses, good_guesses):
+    # take a guess
+    while True:
+        guess = input("Guess a letter: ").lower()
+        
+        if len(guess) != 1:
+                print("Hey! You can only guess a single letter!")
+        elif guess in bad_guesses or guess in good_guesses:
+            print("You've already guessed that letter.")
+        elif not guess.isalpha():
+            print("You can only guess using letters. There are no symbols or numbers.")
+        else:
+            return(guess)
+
+
+def welcome():
+    start = input("Press enter/return to start or Q to quit ".lower())
+    if start == 'q':
+        print('Bye!')
+        sys.exit()
+    else:
+        return True
+
+
+# make a list of words
+words = [
+    "apple",
+    "banana",
+    "watermelon",
+    "papaya",
+    "pineapple",
+    "mango",
+    "mangosteen"
+    "pear",
+    "tomato",
+    "durian",
+    "honeydew",
+    "grapefruit",
+    "blueberry",
+    "melon",
+    "lime",
+    "lemon"
+] 
+
+print("Welcome to Letter Guess!")
+
+done = False
+
+while True:
+    clear()
+    welcome()
+    play(done)
+
